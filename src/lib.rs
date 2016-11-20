b4166b39db53cc41996b3f8fbfe3cca145018604
Andrew Shu <talklittle@gmail.com>
2016-11-20 14:01:26 -0800

    deps: remove gdk-sys and gio-sys

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 extern crate gdk;
-extern crate gdk_sys as gdk_ffi;
-extern crate gio_sys as gio_ffi;
 extern crate glib;
 extern crate glib_sys as glib_ffi;
 extern crate gobject_sys as gobject_ffi;
